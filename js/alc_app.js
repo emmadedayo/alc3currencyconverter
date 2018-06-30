59d54719a31562611dad988c1405f10e0b57ad9a
Adenagbe Emmanuel <emmanzley@gmail.com>
2018-06-30 04:57:18 -1200

    Update alc_app.js

--- js/alc_app.js
+++ js/alc_app.js
@@ -42,7 +42,7 @@ if (navigator.serviceWorker) {
 // registering sw function
 function registerServiceWorker() {
                     // register the service worker
-                    navigator.serviceWorker.register('alc3currencyconverter/alc_sw/sw.js').then(function (sw) {
+                    navigator.serviceWorker.register('/alc_sw/sw.js').then(function (sw) {
                                         // check service worker controller
                                         if (!navigator.serviceWorker.controller) return;
 
